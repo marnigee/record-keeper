e5eb0c088ab1fe5a57ebd09a3b575803c2fc3edc
Marni Goltsman <mgoltsman@bauerxcel.com>
2017-04-30 19:40:49 -0400

    Get elasticSearch working on Heroku with Bonsai

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,5 @@
-require 'elasticsearch/model'
-
 class Album < ActiveRecord::Base
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
 end
-Album.import force: true # for auto sync model with elastic search
+Album.import # for auto sync model with elastic search
